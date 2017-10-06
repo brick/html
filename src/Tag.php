c8fe1b8e4a8fe973435a61a617eeb6bc5471f464
Benjamin Morel <benjamin.morel@gmail.com>
2017-10-06 13:58:30 +0200

    Merge Tag subclasses
    The list of void elements is now built-in, and the presence of the closing tag is automatically inferred from the tag name.

--- src/Tag.php
+++ src/Tag.php
@@ -3,132 +3,267 @@
 namespace Brick\Html;
 
 /**
- * Represents an HTML tag.
+ * An HTML5 tag.
+ *
+ * It is assumed that all texts are UTF-8 encoded.
+ * Tag names and attribute names will be converted to lowercase.
  */
-abstract class Tag
+class Tag
 {
+    /**
+     * The list of void elements, that cannot have any contents.
+     *
+     * @see https://www.w3.org/TR/html51/syntax.html#void-elements
+     */
+    private const VOID_ELEMENTS = [
+        'area',
+        'base',
+        'br',
+        'col',
+        'embed',
+        'hr',
+        'img',
+        'input',
+        'keygen',
+        'link',
+        'menuitem',
+        'meta',
+        'param',
+        'source',
+        'track',
+        'wbr'
+    ];
+
     /**
      * The tag name.
      *
      * @var string
      */
-    protected $name;
+    private $name;
 
     /**
-     * The tag attributes as an associative array.
+     * Whether the tag name matches a void element.
+     *
+     * @var bool
+     */
+    private $isVoid;
+
+    /**
+     * The tag attributes, as an associative array.
      *
      * @var array
      */
-    protected $attributes;
+    private $attributes = [];
 
     /**
-     * Class constructor.
+     * The HTML content.
      *
-     * @param string $name
-     * @param array  $attributes
+     * @var string
      */
-    public function __construct($name, array $attributes = [])
+    private $content = '';
+
+    /**
+     * Tag constructor.
+     *
+     * @param string $name The tag name.
+     */
+    public function __construct(string $name)
     {
-        $this->name       = $name;
-        $this->attributes = $attributes;
+        $name = strtolower($name);
+
+        $this->name   = $name;
+        $this->isVoid = in_array($name, self::VOID_ELEMENTS, true);
     }
 
     /**
+     * Returns the tag name.
+     *
      * @return string
      */
-    public function getName()
+    public function getName() : string
     {
         return $this->name;
     }
 
     /**
+     * Returns the tag attributes, as an associative array.
+     *
      * @return array
      */
-    public function getAttributes()
+    public function getAttributes() : array
     {
         return $this->attributes;
     }
 
     /**
-     * @param string $name
+     * Returns whether the tag has an attribute with the given name.
      *
-     * @return boolean
+     * @param string $name The attribute name.
+     *
+     * @return bool
      */
-    public function hasAttribute($name)
+    public function hasAttribute(string $name) : bool
     {
-        return isset($this->attributes[$name]);
+        return isset($this->attributes[strtolower($name)]);
     }
 
     /**
-     * @param string $name
+     * Returns the value of the attribute with the given name, if any.
      *
-     * @return string|null
+     * @param string $name The attribute name.
+     *
+     * @return string|null The value, or null if there is no such attribute.
      */
-    public function getAttribute($name)
+    public function getAttribute(string $name) : ?string
     {
-        return isset($this->attributes[$name]) ? $this->attributes[$name] : null;
+        return $this->attributes[strtolower($name)] ?? null;
     }
 
     /**
-     * @param string $name
-     * @param string $value
+     * Sets an attribute.
      *
-     * @return static
+     * @param string $name  The attribute name.
+     * @param string $value The attribute value.
+     *
+     * @return Tag This instance, for chaining.
      */
-    public function setAttribute($name, $value)
+    public function setAttribute(string $name, string $value) : Tag
     {
-        $this->attributes[$name] = $value;
+        $this->attributes[strtolower($name)] = $value;
 
         return $this;
     }
 
     /**
-     * @param array $attributes
+     * Sets several attributes.
      *
-     * @return static
+     * @param array $attributes The attributes, as an associative array of names to values.
+     *
+     * @return Tag This instance, for chaining.
      */
-    public function setAttributes(array $attributes)
+    public function setAttributes(array $attributes) : Tag
     {
-        $this->attributes = $attributes + $this->attributes;
+        foreach ($attributes as $name => $value) {
+            $this->attributes[strtolower($name)] = (string) $value;
+        }
 
         return $this;
     }
 
     /**
-     * @param string $name
+     * Removes an attribute.
      *
-     * @return static
+     * If the attribute does not exist, this method does nothing.
+     *
+     * @param string $name The attribute name.
+     *
+     * @return Tag This instance, for chaining.
      */
-    public function removeAttribute($name)
+    public function removeAttribute(string $name) : Tag
     {
-        unset($this->attributes[$name]);
+        unset($this->attributes[strtolower($name)]);
 
         return $this;
     }
 
     /**
+     * @param string $content The text content.
+     *
+     * @return Tag This instance, for chaining.
+     *
+     * @throws \LogicException If this tag is a void element.
+     */
+    public function setTextContent(string $content) : Tag
+    {
+        if ($this->isVoid) {
+            throw new \LogicException('Void elements cannot have any contents.');
+        }
+
+        $this->content = htmlspecialchars($content, ENT_NOQUOTES | ENT_HTML5);
+
+        return $this;
+    }
+
+    /**
+     * @param string $content The HTML content.
+     *
+     * @return Tag This instance, for chaining.
+     *
+     * @throws \LogicException If this tag is a void element.
+     */
+    public function setHtmlContent(string $content) : Tag
+    {
+        if ($this->isVoid) {
+            throw new \LogicException('Void elements cannot have any contents.');
+        }
+
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Returns whether this tag has no content.
+     *
+     * @return bool True if empty, false if this tag has any content.
+     */
+    public function isEmpty() : bool
+    {
+        return $this->content === '';
+    }
+
+    /**
+     * Renders the opening tag.
+     *
      * @return string
      */
-    protected function renderAttributes()
+    public function renderOpeningTag() : string
     {
-        $result = '';
+        $result = '<' . $this->name;
 
         foreach ($this->attributes as $name => $value) {
-            $result .= sprintf(' %s="%s"', $name, htmlspecialchars($value));
+            $result .= sprintf(' %s="%s"', $name, htmlspecialchars($value, ENT_COMPAT | ENT_HTML5));
         }
+
+        $result .= '>';
 
         return $result;
     }
 
     /**
+     * Renders the closing tag.
+     *
+     * @return string
+     *
+     * @throws \LogicException If this tag is a void element.
+     */
+    public function renderClosingTag() : string
+    {
+        if ($this->isVoid) {
+            throw new \LogicException('Void elements do not have a closing tag.');
+        }
+
+        return sprintf('</%s>', $this->name);
+    }
+
+    /**
+     * Renders the tag.
+     *
      * @return string
      */
-    abstract public function render();
+    public function render() : string
+    {
+        if ($this->isVoid) {
+            return $this->renderOpeningTag();
+        }
+
+        return $this->renderOpeningTag() . $this->content . $this->renderClosingTag();
+    }
 
     /**
      * @return string
      */
-    public function __toString()
+    public function __toString() : string
     {
         return $this->render();
     }
